2f04225aa6e5e144dd6cabba3e0ba523c1c9a9a2
deepak <deepak.k9845@gmail.com-at-894833470606>
2020-01-22 15:52:30 +0530

    upload homepage

---
+++ homepage1.php
@@ -0,0 +1,67 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <title>homepage</title>
+<style>
+body {
+      margin:0;
+}
+/* Style the header*/
+.header {
+ text-align:center;
+ font-size: 35px;
+ color: #3385ff;
+}
+
+/* style the top navigation bar */
+.topnav{
+  overflow: hidden;
+  background-color:#bb71b167;
+
+}
+/* style the topnav links*/
+.topnav a{
+float: left;
+display: block;
+color: white;
+text-align: center;
+padding: 40px 35px;
+text-decoration: none;  
+}
+
+/*change color on hover */
+.topnav a:hover{
+  background-color:#808080;
+  color: white ;
+}
+}
+</style>
+</head>
+<body background="iii.jpg">
+<div class="header">
+   <h1>VIDHYA SCHOLAR<br></br></h1>
+   
+</div>
+<div class="topnav ">
+
+    <table >
+        <tr>
+            <td><a href="log1.php">LOGIN</a></td>
+            <td><a href="register.php">REGISTER</a></td>
+            <td><a href="view1.php">SCHOLARSHIPS</a></td>
+            <td><a href="testimonialsview.php">TESTIMONIALS</a></td>
+            <td><a href="adminlogin.php">ADMINLOGIN</a></td>
+            <td><a href="contactus.php">CONTACT US</a></td>         
+			<td><a href="aboutus.php">ABOUT US</a></td>
+
+    </tr>
+</table>
+	</form>		
+
+
+
+</body>
+</html>
+
