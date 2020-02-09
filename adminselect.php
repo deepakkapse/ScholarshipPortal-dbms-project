71622a0ff127e947744575accfd313241fab6e5b
deepakkapse <41117353+deepakkapse@users.noreply.github.com>
2020-02-09 17:38:28 +0530

    upload

---
+++ adminselect.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+<head>
+<title>ABOUT US</title>
+<style>
+.button {
+    background-color:#99ffff;
+    border: none;
+    color:golden;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 10%
+	
+}
+body {
+background-image: url("o.jpg");
+
+}
+</style>
+
+	 <button class="button" value="home" onClick="window.location.href='homepage1.php'" class="css3button" style="margin-left: 40px"  >HOME</button>
+	 
+	    <h2 align="center"><font color="#191970" size="20" face="segeo print">INSERT DETAILS </font> </h2>
+		 
+         <button class="button" value="home" onClick="window.location.href='adminupload1.php'"  style="margin-left:500px"  >Scholarship</button>
+	 <button class="button" value="home" onClick="window.location.href='studenttest.php'" style="margin-left: 40px"  >Awardee</button>
+
+		<P align="center"> hello here, select above one<br>
+		                   
+						   
+		</P>
+</head>
+</html>
